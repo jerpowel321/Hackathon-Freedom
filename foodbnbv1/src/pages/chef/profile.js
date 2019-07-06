c0ff14233c3f25815d98c0574d4c6f1dc2fe39d3
Aryan Varshney <aryanv@gmail.com>
2019-07-06 12:36:06 -0700

    Created basic structure for profile and signup page

---
+++ foodbnbv1/src/pages/chef/profile.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+
+
+const Profile = ({props}) => {
+    <div>
+        <div id='basic-info'>
+            <h1>{props.name}</h1>
+            <p>{props.email}</p>
+            <img src={props.profile_img} alt='Profile Pic'/>
+            <p>{props.bio}</p>
+            <p>Zip Code: {props.zip}</p>
+        </div>
+        <div id='food-info'>
+            <p>Main Cuisine: {props.cuisine}</p>
+            <p>Specialties</p>
+            <ul>
+                {props.specialties.map((special) => (
+                    <li>{special}</li>
+                ))}
+            </ul>
+        </div>
+    </div>
+}
+    
+
+export default Profile
