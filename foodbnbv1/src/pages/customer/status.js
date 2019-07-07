2afa0abeb47b6b941802b04886cc0b77e405fa02
Mel-Marsollier <mmarsollier@outlook.com>
2019-07-07 00:21:30 -0700

    Getting the styling for delivery

--- foodbnbv1/src/pages/customer/status.js
+++ foodbnbv1/src/pages/customer/status.js
@@ -25,13 +25,15 @@ class Status extends Component {
                 </div>
 
                 <div className="text-center">
+                    <br></br>
                     <h3>Checkout</h3>
                     <br></br>
                 </div>
                 <div className="container">
+
                     <div className="receipt">
 
-                        <div className="col-sm-6">
+                        <div className="col-sm-5">
                             <div className="card">
                                 <div className="card-body">
                                     <div className="float-right">
@@ -46,25 +48,72 @@ class Status extends Component {
                             </div>
                         </div>
 
+                        <br></br>
 
-                        {/* <form>
+                        <div className="col-sm-5">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 class="card-title">Select Delivery Time</h5>
 
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
-                                    <label class="form-check-label" for="defaultCheck1">
-                                        Default checkbox
-                                        </label>
-                                </input>
 
+                                    <p>We'll drop it off at your door, eat it fresh, be there to pick it</p>
+
+
+
+                                    <form>
+
+                                        <div className="form-group">
+                                            <h6>1-hour delivery windows</h6>
+
+                                            <div className="form-check">
+                                                <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked></input>
+                                                <label className="form-check-label" for="exampleRadios1">
+                                                    11:00 AM to 12:00 PM
+                                            </label>
+                                            </div>
+
+                                            
+
+
+                                            <div className="form-check">
+                                                <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked></input>
+                                                <label className="form-check-label" for="exampleRadios1">
+                                                    12:00 PM to 13:00 PM
+                                            </label>
+                                            </div>
+
+                                            
+
+                                            <div className="form-check">
+                                                <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked></input>
+                                                <label className="form-check-label" for="exampleRadios1">
+                                                    13:00 PM to 14:00 PM
+                                            </label>
+                                            </div>
+
+                                           
+
+                                            <div className="form-check">
+                                                <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked></input>
+                                                <label className="form-check-label" for="exampleRadios1">
+                                                    14:00 PM to 15:00 PM
+                                            </label>
+
+                                            </div>
+                                        </div>
+
+                                    </form>
+
+                                </div>
                             </div>
-                        </form> */}
+                        </div>
+
 
                     </div>
                 </div>
 
-          
-            <Footer></Footer>
-   
+                <Footer></Footer>
+
 
 
             </div>
@@ -80,27 +129,27 @@ export default Status;
 
 
 
-                //1-hour delivery windows
-                 //4:00 - 6:00 PM
-                //$4.99
-                 //6:00 - 8:00 PM
-                //$4.99
-                 //8:00 - 10:00 PM
-                //$4.99
+                        //1-hour delivery windows
+                         //4:00 - 6:00 PM
+                        //$4.99
+                         //6:00 - 8:00 PM
+                        //$4.99
+                         //8:00 - 10:00 PM
+                        //$4.99
 
 
-                //Tip:  $5.00 (recommended) Edit
+                        //Tip:  $5.00 (recommended) Edit
 
-                //Order Summary
-                //Items (1):	$2.99
-                //Delivery Fee:	-
-                //Total before tax & tip:	$2.99
-                //Tip:	$5.00
+                        //Order Summary
+                        //Items (1):	$2.99
+                        //Delivery Fee:	-
+                        //Total before tax & tip:	$2.99
+                        //Tip:	$5.00
 
-                //Order total: $7.99
+                        //Order total: $7.99
 
-                // Order completed 
-                // Math Floor
+                        // Order completed 
+                        // Math Floor
 
 
 
