5c3ccf278e163ece082e54240745ac8e6deebd2e
Mel-Marsollier <mmarsollier@outlook.com>
2019-07-07 11:28:33 -0700

    Styling of the Summary Page

--- foodbnbv1/src/pages/customer/status.js
+++ foodbnbv1/src/pages/customer/status.js
@@ -25,10 +25,10 @@ class Status extends Component {
 
 
                         <div className="row">
-               <div className="col-sm-6">
+                            <div className="col-sm-6">
                                 <div className="card">
                                     <div className="card-body">
-                                        <div className="inline-block">
+                                        <div className="float-right">
                                             <button type="button" className="btn btn-link flex-end">Edit</button>
                                         </div>
                                         <h5 class="card-title">Delivery Address</h5>
@@ -44,30 +44,49 @@ class Status extends Component {
                                 <div className="card">
                                     <div className="card-body">
                                         <h5>Order Summary</h5>
-                                        <div className="float-center">
-                                        <img className="ml-3 mr-2" id="logo" src="../images/foodlogo.png" alt="logo" />
-                                        </div>
-                                    </div>
-
-                                </div>
-                            </div>
-
-
-
-                            <div className="col-sm-6">
-                                <div className="card">
-                                    <div className="card-body">
+                                        <br></br>
                                         <div className="inline-block">
                                             <img className="ml-3 mr-2" id="logo" src="../images/foodlogo.png" alt="logo" />
+                                            <b><a className="navbar-brand"><span className="green">Food</span><span className="orange">Drop</span></a></b>
                                         </div>
 
-                                        <p>Order Summary</p>
-                                        <p>Items (1):</p>
+                                        <br></br>
+
+                                        <div>
+                                            <h6 className="font-weight-bold">Item(s):</h6>
+                                        </div>
+                                        <div className="row">
+                                            <div className="col-10">
+                                                <h7>- Name1</h7>
+                                            </div>
+                                            <div className="col-2">
+                                                <p className="">$ price1</p>
+                                            </div>
+                                        </div>
+
+                                        <div className="row">
+                                        <div className="col-10">
+                                            <h7>Delivery Fees:</h7>
+                                            </div>
+                                            <div className="col-2">
+                                            <p className="float-right">$ 5.00</p>
+                                            </div>
+                                        </div>
+
+                                        <div>
+                                            <b><h7>Total:</h7></b>
+                                            <p className="float-right">$ Total</p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
 
+                            </div>
                         </div>
+
+
+
+                        {/* <div className="inline-block"> */}
+
 
 
                         {/* <br></br> */}
@@ -81,7 +100,7 @@ class Status extends Component {
 
 
                                     <p>We'll drop it off at your door, eat it fresh, be there to pick it up!</p>
-            <form>
+                                    <form>
                                         <div className="form-group">
                                             <h6>1-hour delivery windows</h6>
                                             <div className="form-check">
@@ -104,7 +123,7 @@ class Status extends Component {
                                                     13:00 PM to 14:00 PM
                                             </label>
                                             </div>
-                          <div className="form-check">
+                                            <div className="form-check">
                                                 <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked></input>
                                                 <label className="form-check-label" for="exampleRadios1">
                                                     14:00 PM to 15:00 PM
